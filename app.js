059bc60dd743405afa9ed98230cbf7bf8788300f
enchichen1107 <b03409032@gmail.com>
2021-06-16 19:55:02 +0800

    feat: connect to db

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// set db connection
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/url-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 // show front page
 app.get('/', (req, res) => {
   res.send('hello world')
---
+++ models/url.js
@@ -0,0 +1,14 @@
+// set url-list schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const urlSchema = new Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: String,
+    required: true
+  }
+})
+module.exports = mongoose.model('Url', urlSchema)
